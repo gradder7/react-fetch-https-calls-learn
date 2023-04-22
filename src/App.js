7a8b44ed75060a359efae024db02f77f362d431d
Himanshu <mehra3228@gmail.com>
2023-04-22 15:04:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import fetchPost from "./utils/fetchPost";
+import { Card, CardContent, Grid, Typography,CircularProgress } from "@mui/material";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetchPost();
+      setPosts(data.posts);
+      setLoading(false)
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {loading ? (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{ height: "calc(100vh - 150px)" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CircularProgress />
+        </Grid>
+      ) : (
+        <>
+          <Typography variant="h4" sx={{ textAlign: "center" }}>
+            List of Posts
+          </Typography>
+          {posts.map((post) => (
+            <Card
+              key={post.id}
+              sx={{ my: 3 }}
+              style={{ backgroundColor:"#faecd9"}}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {post.id}:- {"  "}
+                  {post.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {post.body}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </>
+      )}
+    </>
   );
 }
 
---
+++ src/utils/fetchPost.js
@@ -0,0 +1,6 @@
+const fetchPost = async () => {
+  const response = await fetch(`https://dummyjson.com/posts`);
+  const data = await response.json();
+  return data;
+};
+export default fetchPost;
